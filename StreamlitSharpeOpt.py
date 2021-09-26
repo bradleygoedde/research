f5ab0dbfb768fc544eaafe04f751255e55a5be75
bradleygoedde <84544495+bradleygoedde@users.noreply.github.com>
2021-09-25 21:55:28 -0700

    Update StreamlitSharpeOpt.py

--- StreamlitSharpeOpt.py
+++ StreamlitSharpeOpt.py
@@ -7,7 +7,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import streamlit as st
-pip install openpyxl
+conda install -c anaconda xlrd
 
 st.write("""
 # Sharpe Ratio Maximization App
